7d1c667618855606b6631f967cb085ddc35ef227
Annie <anniehwang428@gmail.com>
2023-01-20 17:07:25 -0500

    added discount tax and total models to test product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,40 @@
 class ProductsController < ApplicationController
-  def one_product
-    @product = Product.first
+  def show
+    @product = Product.find_by(id: params["id"])
 
-    render template: "products/show"
+    render :show
   end
 
-  def all_products
+  def index
     @products = Product.all
-    render template: "products/index"
+    render :index
+  end
+
+  def create
+    @product = Product.create(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+
+    render :show
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.update(
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["image_url"] || @product.image_url,
+      description: params["description"] || @product.description,
+    )
+    render :show
+  end
+
+  def destroy
+    product = Product.find_by(id: params["id"])
+    product.destroy
+    render json: { message: "Product destroyed" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/one_product" => "products#one_product"
-  get "/all_products" => "products#all_products"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
