6250c2351e6602c518cdaa6d734d97cc331d6a84
Annie <anniehwang428@gmail.com>
2023-01-27 14:17:42 -0500

    add sessions create action (login)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-  test "create" do
-    assert_difference "User.count", 1 do
-      post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
-      assert_response 201
-    end
-  end
-
   def create
     user = User.new(
       name: params[:name],
