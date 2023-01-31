f5e5dd270a2461aba36177283d4faf4cf03faeef
Annie <anniehwang428@gmail.com>
2023-01-31 15:14:58 -0500

    adds categories to product partial and fixes test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -19,7 +19,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get "/products/#{Product.first.id}.json"
     assert_response 200
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "description", "created_at", "updated_at", "is_discounted?", "tax", "total", "supplier", "images"], data.keys
+    assert_equal ["id", "name", "price", "description", "created_at", "updated_at", "is_discounted?", "tax", "total", "supplier", "images", "categories"].sort, data.keys.sort
   end
 
   test "create" do
